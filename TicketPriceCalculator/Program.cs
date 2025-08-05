067c779dda26660af874f61ada598336a9671385
kingsark500 <kingsark500@gmail.com>
2025-08-05 12:19:28 +0000

    Created ticket price calculator

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class MaxTicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        string input = Console.ReadLine();
+
+        int age;
+
+        if (!int.TryParse(input, out age) || age < 0)
+        {
+
+            Console.WriteLine("Invalid input. Please enter a valid non-negative age.");
+
+            return;
+
+        }
+
+        int ticketPrice;
+
+        if (age >= 65 || age <= 12)
+        {
+
+            ticketPrice = 7;
+
+        }
+
+        else
+        {
+
+            ticketPrice = 10;
+
+        }
+
+        Console.WriteLine($"The ticket price is: GHC{ticketPrice}");
+
+        Console.ReadLine();
+    }
+}
