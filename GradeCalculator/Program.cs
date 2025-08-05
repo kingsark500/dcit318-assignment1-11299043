59f2e531a2e5d7655eb043abc370d1a4d1cafc5e
kingsark500 <kingsark500@gmail.com>
2025-08-05 12:06:31 +0000

    created grade calculator file

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class MaxGradeCalculator
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a numerical grade betwwen 0 and 100:");
+
+        string input = Console.ReadLine();
+        int grade;
+
+        if (!int.TryParse(input, out grade) || grade < 0 || grade > 100)
+        {
+            Console.WriteLine("Invalid input, write a number between 1 and 100");
+            return;
+        }
+
+        char letterGrade;
+
+        if (grade >= 90)
+        {
+            letterGrade = 'A';
+        }
+
+        else if (grade >= 80)
+        {
+            letterGrade = 'B';
+        }
+
+        else if (grade >= 70)
+        {
+            letterGrade = 'C';
+        }
+
+        else
+        {
+            letterGrade = 'F';
+        }
+
+        Console.WriteLine($"The grade is : {letterGrade}");
+
+        Console.ReadLine();
+    }
+}
